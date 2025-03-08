67be919cd4660f4dd8c70aa606103f7b4db1e58a
Madhur Patil <madhurpatilofficial@gmail.com>
2025-03-08 21:16:54 +0530

    Clock Functionality bug fixes

--- src/app/services/world-service.service.ts
+++ src/app/services/world-service.service.ts
@@ -1,22 +1,34 @@
-// world-time.service.ts
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Observable, throwError, of } from 'rxjs';
+import { catchError, map, shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorldTimeService {
+  private apiUrl = 'https://timeapi.io/api/Time/current/zone?timeZone=';
+  private cache: { [key: string]: Observable<any> } = {};
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getTimeByCountry(countryCode: string): Observable<any> {
-    return this.http.get(`https://worldtimeapi.org/api/timezone/${countryCode}`).pipe(
+    if (this.cache[countryCode]) {
+      return this.cache[countryCode];
+    }
+
+    this.cache[countryCode] = this.http.get(`${this.apiUrl}${countryCode}`).pipe(
+      map((data: any) => ({
+        datetime: data.dateTime,
+        timezone: data.timeZone
+      })),
       catchError(error => {
-        return throwError('Failed to fetch time');
-      })
+        console.error('API error:', error);
+        return throwError(() => new Error('Failed to fetch time'));
+      }),
+      shareReplay(1) // Cache the response
     );
+
+    return this.cache[countryCode];
   }
 }
